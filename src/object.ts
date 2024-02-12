5fa68a46cb9a700bf23c5c243c08813e999b86f1
huytrinh <dinhhuy.trinh@aiven.io>
2024-02-12 16:25:47 +0200

    feat: add optional

--- src/object.ts
+++ src/object.ts
@@ -1,12 +1,25 @@
+import { Object } from "ts-toolbelt";
 
 type Filter<O extends object, M> = {
     [K in keyof O as O[K] extends M ? never : K]: O[K]
 }
 
-type FilterKeys<O extends object, M> = {
+type FilterKeys<O extends object, M extends keyof O> = {
     [K in keyof O as K extends M ?  never : K]: O[K]
 }
 
+type OptionalFlat<O> = {
+    [Key in keyof O]?: O[Key]
+}
+
+type OptionalDeep<O> = {
+    [Key in keyof O]?: OptionalDeep<O[Key]>
+}
+
+type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+
+type opt = Object.Optional<User, string>
 
 // Example usages
  
@@ -45,3 +58,15 @@ type User = {
 };
 */
 type filterKey = FilterKeys<User, 'name' | 'age'>
+
+
+/*
+Result: 
+
+type User = {
+    name?: string
+    age?: number
+    email?: string
+};
+*/
+type optionalFlat = OptionalFlat<User>
