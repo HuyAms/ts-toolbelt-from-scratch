79285d9e8c5b13fb7eabf0fc56beecabefef4c0f
huytrinh <dinhhuy.trinh@aiven.io>
2024-02-12 17:52:19 +0200

    feat: add Replace

--- src/string.ts
+++ src/string.ts
@@ -2,4 +2,19 @@ import {String} from 'ts-toolbelt'
 
 type Split<S, D extends string> = S extends `${infer FirstPart}${D}${infer SecondPart}` ? [FirstPart, SecondPart] : never
 
+type Replace<
+  Text extends string,
+  From extends string,
+  To extends string
+> = Text extends `${infer Beginning}${From}` 
+ ?  `${Beginning}${To}`
+ : Text extends `${From}${infer End}` 
+ ? `${To}${End}`
+ : Text extends `${infer Beginning}${From}${infer End}` 
+ ? `${Beginning}${To}${End}` : never
+
+
 type Result = Split<'hello:world', ':'>
+type ReplaceString = Replace<'NewHelloWorld', 'World', 'Test'> // NewHelloTest
+type ReplaceString1 = Replace<'NewHelloWorld', 'Hello', 'Test'> // NewTestWorld
+type ReplaceString2 = Replace<'NewHelloWorld', 'New', 'Test'> // TestHelloWorld
